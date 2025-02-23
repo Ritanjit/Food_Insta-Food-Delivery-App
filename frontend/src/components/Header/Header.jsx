b0a461bb11e36bb13a83c0b43f16847fa53758b1
ritnajit <ritanjitdas@gmail.com>
2025-02-23 17:52:18 +0530

    navigationNavbar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import './Header.css'
-import {Link} from "react-router-dom"
 
 const reloadPage = () => {
   window.location.reload();
 }
 
 const Header = () => {
+
   return (
     <div className='header'>
-        <div className="header-contents">
-            <h2>Order your favourite food here</h2>
-            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your craving and elevate your dining experience, one delicious meal at a time.</p>
-            <button onClick={reloadPage}>View Menu</button>
-        </div>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your craving and elevate your dining experience, one delicious meal at a time.</p>
+        <button onClick={reloadPage}>View Menu</button>
+      </div>
     </div>
   )
 }
